2c79c19b88bbd0734a7fe749898b44425f44d45c
c0ffee0wl <c0ffee0wl@gmx.net>
2025-10-10 14:59:40 -0400

    Tweaked OS environment detection

--- llm_cmd_comp/__init__.py
+++ llm_cmd_comp/__init__.py
@@ -14,7 +14,6 @@ Return only the command to be executed as a raw string, no string delimiters wra
 
 Environment: $shell_display on $os_display$env_suffix$pkg_suffix
 
-Ensure the output is a valid $shell_display command.
 If there is a lack of details, provide the most logical solution.
 If multiple steps are required, try to combine them using '&&' (For PowerShell, use ';' instead).
 
